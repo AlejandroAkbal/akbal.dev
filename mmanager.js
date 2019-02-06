efa914e3dc42ee3c6e7c6f0d508d8547a16ce1ca
Alejandro Romero <37181533+VoidlessSeven7@users.noreply.github.com>
2019-02-06 11:42:07 +0100

    Add files via upload

---
+++ mmanager.js
@@ -0,0 +1,19 @@
+/*var ref = $('.my-popper-object');
+var popup = $('.my-popper');
+popup.hide();
+
+ref.click(function(){
+
+    console.log("esto es una mierda");
+    popup.show();
+    var popper = new Popper(ref, popup, {
+        placement: 'bottom',
+        modifiers: {
+        flip: {
+            behavior: ['left', 'bottom', 'top']
+        },
+    },
+
+    });
+    
+});*/
