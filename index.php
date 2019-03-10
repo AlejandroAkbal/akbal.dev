89e783dafa411f115e2c347c0e1bbf0d838f3240
Alejandro Romero <37181533+VoidlessSeven7@users.noreply.github.com>
2019-03-10 11:41:59 +0000

    Actualizacion
    He añadido que las dependencias sean locales, que sean insertadas por php con include, y bueno, todos los cambios que no habia subido

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="es">
+
+<head>
+
+    <?php include '/webroot/akbal.tk/php/dependenciasCSS.php'; ?>
+
+    <!-- Mi CSS -->
+    <link rel="stylesheet" href="css/index.css">
+
+    <title>Akbal.tk</title>
+
+</head>
+
+<body>
+
+    <div class="bg view">
+
+        <div id="jumbo" class="jumbotron text-center shadow">
+
+            <div class="display-4"><a class="text-light" href="images/logo2000x2500.png">Akbal.tk</a></div>
+
+            <br><kbd class="small text-info"><a href="/webmin">En construccion</a></kbd><br><br>
+
+            <h4 class="text-dark-50">Puedes ir a la pagina vieja <a href="https://old.akbal.tk/">desde este enlace</a>.</h4>
+
+        </div>
+
+    </div>
+
+    <?php include '/webroot/akbal.tk/php/dependenciasJS.php'; ?>
+
+</body>
+
+</html>
