4984d42b24cd3dfe21df15f16eabb019c406bf41
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-08 15:36:22 +0100

    chore: blank slate

--- src/php/dependenciasJS.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo '<!-- JavaScript -->
-    <script src="/dependencies/jquery-3.3.1.slim.min.js"></script>
-    <script src="/dependencies/popper.min.js"></script>
-    <script src="/dependencies/bootstrap.min.js"></script>'
-
-?>
--- src/php/sendServer.php.DEPRECATED
+++
@@ -1,4 +0,0 @@
-<?php
-$output = shell_exec('/scripts/stop.sh $comando 2>&1'); 
-echo "<pre>$output</pre>";
-?>
