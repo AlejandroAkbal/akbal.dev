8466c0b191350c1623295ba4661f9303566caf69
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-09 17:01:52 +0100

    build: minify html

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 
+import { minifyHtml, injectHtml } from 'vite-plugin-html'
+
 export default defineConfig({
   root: 'src/',
 
   build: {
     outDir: '../dist',
   },
+
+  plugins: [minifyHtml()],
 })
