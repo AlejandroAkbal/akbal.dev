85d9477896e99b97221ca97e42c416e3bbf29ff4
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-09 17:13:14 +0100

    fix: CSS fixes and improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -132,15 +132,11 @@ module.exports = {
       colors: {
         // primary: generatedColors.orchid,
 
-        'a-black-100': 'hsl(200, 6%, 18%)',
-        'a-black-200': 'hsl(200, 6%, 16%)',
-        'a-black-300': 'hsl(200, 6%, 14%)',
-        'a-black-400': 'hsl(200, 6%, 12%)',
-        'a-black-500': 'hsl(200, 6%, 10%)', // OG
-        'a-black-600': 'hsl(200, 6%, 8%)',
-        'a-black-700': 'hsl(200, 6%, 6%)',
-        'a-black-800': 'hsl(200, 6%, 4%)',
-        'a-black-900': 'hsl(200, 6%, 2%)',
+        'a-black': {
+          300: 'hsl(200, 6%, 14%)',
+          500: 'hsl(200, 6%, 10%)', // OG
+          700: 'hsl(200, 6%, 6%)',
+        },
 
         'a-violet-500': 'hsl(320, 42%, 47%)',
 
