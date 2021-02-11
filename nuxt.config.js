bdf3d3d5806122c935122fcbccf2aef25340b29e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-11 10:01:48 +0100

    feat: configure nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,17 +2,50 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  ssr: true,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'akbal-dev',
+    title: 'Alejandro Akbal - Software developer',
+
     htmlAttrs: {
       lang: 'en',
     },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Alejandro Akbal is a software developer. He enjoys many things: web development, design, accessibility, SEO, servers, tutorials, and everything in between.',
+      },
+
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Alejandro Akbal',
+      },
+      {
+        hid: 'og:type',
+        name: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content:
+          'https://og-image.akbal.dev/**Alejandro Akbal** Software developer.png?theme=light&md=1&fontSize=125px&widths=350&heights=350',
+      },
+      {
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://akbal.dev',
+      },
     ],
+
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
@@ -27,8 +60,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
