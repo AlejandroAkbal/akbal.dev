aeefff03b71a848ed2b2d289f5e928e073167828
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-11 11:23:25 +0100

    feat: create SocialIcon component

---
+++ components/pages/index/SocialIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <li
+    class="p-2 border border-transparent rounded-full shadow-sm bg-a-black-700 focus-within:border-orchid-500"
+  >
+    <a
+      :href="href"
+      class="focus:outline-none group"
+      target="_blank"
+      rel="noopener"
+      :title="title"
+    >
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-6 h-6 text-white transition-colors group-hover:text-orchid-500 group-focus:text-orchid-500"
+      >
+        <slot></slot>
+      </svg>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: { title: String, href: String },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -80,115 +80,42 @@
 
               <ul class="flex flex-wrap justify-center gap-4 mt-4">
                 <!-- Blog -->
-                <li
-                  class="p-2 border border-transparent rounded-full shadow-sm bg-a-black-700"
-                >
-                  <a
-                    href="https://redirect.akbal.dev/blog"
-                    class="group"
-                    target="_blank"
-                    rel="noopener"
-                    title="Blog"
-                  >
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="1"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="w-6 h-6 text-white transition-colors group-hover:text-orchid-500 group-focus:text-orchid-500"
-                    >
-                      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                      <path
-                        d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"
-                      ></path>
-                    </svg>
-                  </a>
-                </li>
+                <SocialIcon title="Blog" href="https://redirect.akbal.dev/blog">
+                  <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                  <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                </SocialIcon>
 
                 <!-- GitHub -->
-                <li
-                  class="p-2 border border-transparent rounded-full shadow-sm bg-a-black-700"
+                <SocialIcon
+                  title="GitHub"
+                  href="https://redirect.akbal.dev/github"
                 >
-                  <a
-                    href="https://redirect.akbal.dev/github"
-                    class="group"
-                    target="_blank"
-                    rel="noopener"
-                    title="GitHub"
-                  >
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="1"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="w-6 h-6 text-white transition-colors group-hover:text-orchid-500 group-focus:text-orchid-500"
-                    >
-                      <path
-                        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-                      ></path>
-                    </svg>
-                  </a>
-                </li>
+                  <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                  ></path>
+                </SocialIcon>
 
                 <!-- Twitter -->
-                <li
-                  class="p-2 border border-transparent rounded-full shadow-sm bg-a-black-700"
+                <SocialIcon
+                  title="Twitter"
+                  href="https://redirect.akbal.dev/twitter"
                 >
-                  <a
-                    href="https://redirect.akbal.dev/twitter"
-                    class="group"
-                    target="_blank"
-                    rel="noopener"
-                    title="Twitter"
-                  >
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="1"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="w-6 h-6 text-white transition-colors group-hover:text-orchid-500 group-focus:text-orchid-500"
-                    >
-                      <path
-                        d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                      ></path>
-                    </svg>
-                  </a>
-                </li>
+                  <path
+                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+                  ></path>
+                </SocialIcon>
 
                 <!-- LinkedIn -->
-                <li
-                  class="p-2 border border-transparent rounded-full shadow-sm bg-a-black-700"
+                <SocialIcon
+                  title="LinkedIn"
+                  href="https://redirect.akbal.dev/linkedin"
                 >
-                  <a
-                    href="https://redirect.akbal.dev/linkedin"
-                    class="group"
-                    target="_blank"
-                    rel="noopener"
-                    title="LinkedIn"
-                  >
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="1"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="w-6 h-6 text-white transition-colors group-hover:text-orchid-500 group-focus:text-orchid-500"
-                    >
-                      <path
-                        d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-                      ></path>
-                      <rect x="2" y="9" width="4" height="12"></rect>
-                      <circle cx="4" cy="4" r="2"></circle>
-                    </svg>
-                  </a>
-                </li>
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  ></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle>
+                </SocialIcon>
               </ul>
             </div>
 
