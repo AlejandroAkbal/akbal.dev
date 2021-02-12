31d81ff25dcd81367ba8e69b4c650323bb6a22fe
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-12 21:30:31 +0100

    feat: create projects layout

---
+++ layouts/projects.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <div class="container__outer" style="padding-top: 0px; padding-bottom: 0px">
+      <div class="p-4 container__inner">
+        <NuxtLink to="/" class="text-lg link"> &larr; Go back </NuxtLink>
+      </div>
+    </div>
+
+    <Spacer />
+
+    <!-- Content -->
+    <div class="flex-auto">
+      <Nuxt />
+    </div>
+
+    <Spacer />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return { bodyAttrs: { class: ['subpixel-antialiased ', 'bg-a-black-500'] } }
+  },
+}
+</script>
