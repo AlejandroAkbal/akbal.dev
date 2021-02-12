38a5acb1a0d9563f143ff79d24aa8ed3ac801979
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-12 21:31:17 +0100

    feat: create projects navigation footer

---
+++ components/pages/projects/NavigationFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-around">
+    <template v-if="prev">
+      <div class="flex flex-col items-center">
+        <h3 class="text-gray-200">Previous project</h3>
+
+        <NuxtLink :to="prev.path" class="link">
+          {{ prev.title }}
+        </NuxtLink>
+      </div>
+    </template>
+
+    <template v-if="next">
+      <div class="flex flex-col items-center">
+        <h3 class="text-gray-200">Next project</h3>
+
+        <NuxtLink :to="next.path" class="link">
+          {{ next.title }}
+        </NuxtLink>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prev: { default: null, type: Object },
+    next: { default: null, type: Object },
+  },
+}
+</script>
